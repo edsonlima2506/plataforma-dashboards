6a71694aff00dd0001fd55f2f909976d4303ff03
Edson Lima <edsoneduardo526@gmail.com>
2021-12-13 23:44:49 -0300

    Inicia estilização da tela de login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,22 @@
+import '../styles/login.css';
+
 export default function Login() {
     return(
     <div className="loginPage">
-        <form>
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Senha" />
-            <button type="button">Entrar</button>
+        <div className="leftPart">
+            <div className="greenLayer"/>
+            <div className="warning">
+                <p>
+                    Não permita que pessoas que não são colaboradoras <br />
+                    Tenham acesso a esta plataforma, preze por nossos dados.
+                </p>
+            </div>
+        </div>
+        <form className="loginArea">
+            <h1 className="loginTittle">Login</h1>
+            <input type="email" placeholder="Email" className="loginInput" />
+            <input type="password" placeholder="Senha" className="loginInput" />
+            <button type="button" className="loginButton">Entrar</button>
         </form>
     </div>);
 }
