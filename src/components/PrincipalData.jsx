08bc0c7a9ec87bf432e04af0df4a6ab67cd980d9
Edson Lima <edsoneduardo526@gmail.com>
2021-12-15 00:39:38 -0300

    Cria area de principais dados na tela inicial

---
+++ src/components/PrincipalData.jsx
@@ -0,0 +1,9 @@
+import '../styles/principalData.css';
+
+export default function PrincipalData() {
+    return(
+        <section className="principalData">
+            <h1>Dados principais</h1>
+        </section>
+    );
+}
--- src/pages/PrincipalPage.jsx
+++ src/pages/PrincipalPage.jsx
@@ -1,12 +1,14 @@
 import '../styles/principal.css';
 import Header from '../components/Header';
 import SideMenu from '../components/SideMenu';
+import PrincipalData from '../components/PrincipalData';
 
 export default function Principal() {
     return(
         <div className="principalPage">
             <Header />
             <SideMenu />
+            <PrincipalData />
         </div>
     );
 }
