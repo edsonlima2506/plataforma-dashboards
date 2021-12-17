1684f6874c91c68d770f4be536d4a5abb4ee9a90
Edson Lima <edsoneduardo526@gmail.com>
2021-12-17 19:03:01 -0300

    Cria tela usuários

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,3 +1,12 @@
+import '../styles/users.css';
+import SideMenu from '../components/SideMenu';
+import Header from '../components/Header';
+
 export default function Users() {
-    return(<div>Users</div>);
+    return(
+    <div className="usersPage">
+        <Header />
+        <SideMenu />
+    </div>
+    );
 }
